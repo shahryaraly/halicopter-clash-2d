da33364627e339790b49adee527a78206ac0be01
shahryaraly <shahryaraly@live.com>
2022-09-17 12:04:13 +0400

    initial commit - 2015

---
+++ HalicopterGame/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace HalicopterGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timr_bg = new System.Windows.Forms.Timer(this.components);
+            this.timr_enemy = new System.Windows.Forms.Timer(this.components);
+            this.timr_fire = new System.Windows.Forms.Timer(this.components);
+            this.lbl_fire = new System.Windows.Forms.Label();
+            this.lbl_enemy1 = new System.Windows.Forms.Label();
+            this.lbl_enemy2 = new System.Windows.Forms.Label();
+            this.lbl_enemy6 = new System.Windows.Forms.Label();
+            this.lbl_enemy3 = new System.Windows.Forms.Label();
+            this.lbl_enemy4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_crash = new System.Windows.Forms.Label();
+            this.enemy1_collision = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_points = new System.Windows.Forms.Label();
+            this.lbl_escapepoints = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hero_collsion = new System.Windows.Forms.Timer(this.components);
+            this.lbl_chances = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_hero = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timr_bg
+            // 
+            this.timr_bg.Enabled = true;
+            this.timr_bg.Interval = 20;
+            this.timr_bg.Tick += new System.EventHandler(this.timr_bg_Tick_1);
+            // 
+            // timr_enemy
+            // 
+            this.timr_enemy.Enabled = true;
+            this.timr_enemy.Interval = 50;
+            this.timr_enemy.Tick += new System.EventHandler(this.timr_enemy_Tick);
+            // 
+            // timr_fire
+            // 
+            this.timr_fire.Enabled = true;
+            this.timr_fire.Interval = 10;
+            this.timr_fire.Tick += new System.EventHandler(this.timr_fire_Tick);
+            // 
+            // lbl_fire
+            // 
+            this.lbl_fire.AutoSize = true;
+            this.lbl_fire.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_fire.Image = global::HalicopterGame.Properties.Resources.bullet;
+            this.lbl_fire.Location = new System.Drawing.Point(489, 288);
+            this.lbl_fire.Name = "lbl_fire";
+            this.lbl_fire.Padding = new System.Windows.Forms.Padding(20, 5, 35, 5);
+            this.lbl_fire.Size = new System.Drawing.Size(55, 23);
+            this.lbl_fire.TabIndex = 10;
+            // 
+            // lbl_enemy1
+            // 
+            this.lbl_enemy1.AutoSize = true;
+            this.lbl_enemy1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_enemy1.Image = global::HalicopterGame.Properties.Resources.enemy;
+            this.lbl_enemy1.Location = new System.Drawing.Point(500, 100);
+            this.lbl_enemy1.Name = "lbl_enemy1";
+            this.lbl_enemy1.Padding = new System.Windows.Forms.Padding(30, 20, 40, 10);
+            this.lbl_enemy1.Size = new System.Drawing.Size(70, 43);
+            this.lbl_enemy1.TabIndex = 9;
+            // 
+            // lbl_enemy2
+            // 
+            this.lbl_enemy2.AutoSize = true;
+            this.lbl_enemy2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_enemy2.Image = global::HalicopterGame.Properties.Resources.enemy;
+            this.lbl_enemy2.Location = new System.Drawing.Point(500, 200);
+            this.lbl_enemy2.Name = "lbl_enemy2";
+            this.lbl_enemy2.Padding = new System.Windows.Forms.Padding(30, 20, 40, 10);
+            this.lbl_enemy2.Size = new System.Drawing.Size(70, 43);
+            this.lbl_enemy2.TabIndex = 7;
+            // 
+            // lbl_enemy6
+            // 
+            this.lbl_enemy6.AutoSize = true;
+            this.lbl_enemy6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_enemy6.Image = global::HalicopterGame.Properties.Resources.enemy;
+            this.lbl_enemy6.Location = new System.Drawing.Point(650, 150);
+            this.lbl_enemy6.Name = "lbl_enemy6";
+            this.lbl_enemy6.Padding = new System.Windows.Forms.Padding(30, 20, 40, 10);
+            this.lbl_enemy6.Size = new System.Drawing.Size(70, 43);
+            this.lbl_enemy6.TabIndex = 5;
+            // 
+            // lbl_enemy3
+            // 
+            this.lbl_enemy3.AutoSize = true;
+            this.lbl_enemy3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_enemy3.Image = global::HalicopterGame.Properties.Resources.enemy;
+            this.lbl_enemy3.Location = new System.Drawing.Point(500, 300);
+            this.lbl_enemy3.Name = "lbl_enemy3";
+            this.lbl_enemy3.Padding = new System.Windows.Forms.Padding(30, 20, 40, 10);
+            this.lbl_enemy3.Size = new System.Drawing.Size(70, 43);
+            this.lbl_enemy3.TabIndex = 3;
+            // 
+            // lbl_enemy4
+            // 
+            this.lbl_enemy4.AutoSize = true;
+            this.lbl_enemy4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_enemy4.Image = global::HalicopterGame.Properties.Resources.enemy;
+            this.lbl_enemy4.Location = new System.Drawing.Point(550, 250);
+            this.lbl_enemy4.Name = "lbl_enemy4";
+            this.lbl_enemy4.Padding = new System.Windows.Forms.Padding(30, 20, 40, 10);
+            this.lbl_enemy4.Size = new System.Drawing.Size(70, 43);
+            this.lbl_enemy4.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::HalicopterGame.Properties.Resources.hi_img01;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbl_crash
+            // 
+            this.lbl_crash.AutoSize = true;
+            this.lbl_crash.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_crash.Image = global::HalicopterGame.Properties.Resources.crash;
+            this.lbl_crash.Location = new System.Drawing.Point(682, 235);
+            this.lbl_crash.Name = "lbl_crash";
+            this.lbl_crash.Padding = new System.Windows.Forms.Padding(30, 25, 20, 20);
+            this.lbl_crash.Size = new System.Drawing.Size(50, 58);
+            this.lbl_crash.TabIndex = 11;
+            // 
+            // enemy1_collision
+            // 
+            this.enemy1_collision.Enabled = true;
+            this.enemy1_collision.Interval = 1;
+            this.enemy1_collision.Tick += new System.EventHandler(this.enemy1_collision_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Points : ";
+            // 
+            // lbl_points
+            // 
+            this.lbl_points.AutoSize = true;
+            this.lbl_points.Location = new System.Drawing.Point(268, 377);
+            this.lbl_points.Name = "lbl_points";
+            this.lbl_points.Size = new System.Drawing.Size(0, 13);
+            this.lbl_points.TabIndex = 13;
+            // 
+            // lbl_escapepoints
+            // 
+            this.lbl_escapepoints.AutoSize = true;
+            this.lbl_escapepoints.Location = new System.Drawing.Point(130, 377);
+            this.lbl_escapepoints.Name = "lbl_escapepoints";
+            this.lbl_escapepoints.Size = new System.Drawing.Size(0, 13);
+            this.lbl_escapepoints.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Escape Points";
+            // 
+            // hero_collsion
+            // 
+            this.hero_collsion.Enabled = true;
+            this.hero_collsion.Interval = 10;
+            this.hero_collsion.Tick += new System.EventHandler(this.hero_collsion_Tick);
+            // 
+            // lbl_chances
+            // 
+            this.lbl_chances.AutoSize = true;
+            this.lbl_chances.Location = new System.Drawing.Point(390, 377);
+            this.lbl_chances.Name = "lbl_chances";
+            this.lbl_chances.Size = new System.Drawing.Size(0, 13);
+            this.lbl_chances.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Chances  : ";
+            // 
+            // lbl_hero
+            // 
+            this.lbl_hero.AutoSize = true;
+            this.lbl_hero.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_hero.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_hero.Image = global::HalicopterGame.Properties.Resources.hero;
+            this.lbl_hero.Location = new System.Drawing.Point(24, 180);
+            this.lbl_hero.Name = "lbl_hero";
+            this.lbl_hero.Padding = new System.Windows.Forms.Padding(30, 20, 40, 10);
+            this.lbl_hero.Size = new System.Drawing.Size(70, 43);
+            this.lbl_hero.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 399);
+            this.Controls.Add(this.lbl_chances);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_escapepoints);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_points);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_crash);
+            this.Controls.Add(this.lbl_fire);
+            this.Controls.Add(this.lbl_enemy1);
+            this.Controls.Add(this.lbl_enemy2);
+            this.Controls.Add(this.lbl_enemy6);
+            this.Controls.Add(this.lbl_enemy3);
+            this.Controls.Add(this.lbl_enemy4);
+            this.Controls.Add(this.lbl_hero);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timr_bg;
+        private System.Windows.Forms.Label lbl_enemy4;
+        private System.Windows.Forms.Label lbl_enemy3;
+        private System.Windows.Forms.Label lbl_enemy6;
+        private System.Windows.Forms.Label lbl_enemy2;
+        private System.Windows.Forms.Label lbl_enemy1;
+        private System.Windows.Forms.Timer timr_enemy;
+        private System.Windows.Forms.Label lbl_fire;
+        private System.Windows.Forms.Timer timr_fire;
+        private System.Windows.Forms.Label lbl_crash;
+        private System.Windows.Forms.Timer enemy1_collision;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_points;
+        private System.Windows.Forms.Label lbl_escapepoints;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer hero_collsion;
+        private System.Windows.Forms.Label lbl_chances;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_hero;
+    }
+}
+
---
+++ HalicopterGame/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace HalicopterGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        int p = 0, moveleft, movefire, points = 0, enemyescape = 0, chances = 0;
+        int a = 500, b = 500, c = 500, d = 550, g = 550;
+        int fire_show = 0;
+        Boolean left, show, declare1, declare2, declare3, declare4, declare5;
+        SoundPlayer helicopter = new SoundPlayer();
+        SoundPlayer explod = new SoundPlayer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 510;
+            this.Height = 450;
+            movefire = lbl_fire.Location.X;
+            lbl_fire.Location = new Point(lbl_hero.Location.X, lbl_hero.Location.Y + 20);
+            lbl_fire.Hide();
+            timr_fire.Stop();
+            lbl_crash.Hide();
+            helicopter.SoundLocation = @"hlcptr_sound.wav";
+            // helicopter.SoundLocation = @"hlcptr_sound.wav";
+            helicopter.PlayLooping();
+            explod.SoundLocation = @"explod.wav";
+        }
+
+        private void timr_bg_Tick_1(object sender, EventArgs e)
+        {
+
+            p = p - 10;
+            pictureBox1.Location = new Point(p, 0);
+
+            if (p == -500)
+            {
+                pictureBox1.Location = new Point(0, 0);
+                p = 0;
+                p = p - 10;
+                pictureBox1.Location = new Point(p, 0);
+            }
+        }
+        private void timr_enemy_Tick(object sender, EventArgs e)
+        {
+
+
+            moveleft -= 2;
+            if (left == true && lbl_hero.Location.X > 24)
+            {
+                lbl_hero.Location = new Point(moveleft, lbl_hero.Location.Y);
+                if (moveleft < 24)
+                {
+                    left = false;
+                }
+            }
+            a = a - 3;
+            b = b - 2;
+            c = c - 3;
+            d = d - 2;
+            g = g - 3;
+
+            lbl_enemy1.Location = new Point(a, 100);
+            lbl_enemy2.Location = new Point(b, 200);
+            lbl_enemy3.Location = new Point(c, 300);
+            lbl_enemy4.Location = new Point(d, 250);
+            lbl_enemy6.Location = new Point(g, 150);
+
+
+
+
+
+            if (a < -60)
+            {
+                enemyescape++;
+                a = 500;
+            }
+            if (declare1 == true)
+            {
+                declare1 = false;
+                a = 500;
+            }
+            if (b < -60)
+            {
+                enemyescape++;
+                b = 500;
+            }
+            if (declare2 == true)
+            {
+                b = 500;
+                declare2 = false;
+            }
+            if (c < -60)
+            {
+                enemyescape++;
+                c = 500;
+            }
+            if (declare3 == true)
+            {
+                c = 500;
+                declare3 = false;
+            }
+
+            if (d < -60)
+            {
+                enemyescape++;
+                d = 550;
+            }
+            if (declare4 == true)
+            {
+                d = 550;
+                declare4 = false;
+            }
+            if (g < -60)
+            {
+                enemyescape++;
+                g = 550;
+            }
+            if (declare5 == true)
+            {
+                g = 550;
+                declare5 = false;
+            }
+            lbl_escapepoints.Text = "" + enemyescape;
+
+
+
+
+
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = lbl_hero.Location.X;
+            int y = lbl_hero.Location.Y;
+            if (e.KeyCode == Keys.Up && y > 5)
+            {
+                lbl_hero.Location = new Point(x, y - 2);
+            }
+            else if (e.KeyCode == Keys.Down && y < pictureBox1.Height - 50)
+            {
+                lbl_hero.Location = new Point(x, y + 2);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                lbl_hero.Location = new Point(x + 2, y);
+                left = true;
+                moveleft = lbl_hero.Location.X;
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                lbl_fire.Location = lbl_hero.Location;
+                movefire = lbl_hero.Location.X + 40;
+                lbl_fire.Hide();
+                timr_fire.Start();
+                lbl_fire.Show();
+            }
+        }
+        private void timr_fire_Tick(object sender, EventArgs e)
+        {
+            int x = lbl_fire.Location.X;
+            int y = lbl_fire.Location.Y;
+            movefire = movefire + 1;
+            lbl_fire.Location = new Point(movefire, y);
+            if (movefire > this.Width - 20)
+            {
+                lbl_fire.Location = new Point(lbl_hero.Location.X, lbl_hero.Location.Y);
+                lbl_fire.Hide();
+                timr_fire.Stop();
+            }
+        }
+        private void enemy1_collision_Tick(object sender, EventArgs e)
+        {
+            int x = lbl_fire.Location.X;
+            int y = lbl_fire.Location.Y;
+            Text = "X : " + x + " Y : " + y;
+            if ((x + 40 > lbl_enemy1.Location.X) && (y > 85 && y < 138))
+            {
+                explod.Play();
+                declare1 = true;
+                lbl_crash.Location = new Point(x, y - 25);
+                lbl_fire.Location = new Point(lbl_hero.Location.X, lbl_hero.Location.Y);
+                timr_fire.Stop();
+                lbl_fire.Hide();
+                show = true;
+                points++;
+            }
+            if ((x + 40 > lbl_enemy2.Location.X) && y > 185 && y < 235)
+            {
+                explod.Play();
+                declare2 = true;
+                lbl_crash.Location = new Point(x, y - 25);
+                lbl_fire.Location = new Point(lbl_hero.Location.X, lbl_hero.Location.Y);
+                timr_fire.Stop();
+                lbl_fire.Hide();
+                show = true;
+                points++;
+            }
+            if ((x + 40 > lbl_enemy3.Location.X) && y > 287 && y < 327)
+            {
+                explod.Play();
+                declare3 = true;
+                lbl_crash.Location = new Point(x, y - 25);
+                lbl_fire.Location = new Point(lbl_hero.Location.X, lbl_hero.Location.Y);
+                timr_fire.Stop();
+                lbl_fire.Hide();
+                show = true;
+                points++;
+            }
+            if ((x + 40 > lbl_enemy4.Location.X) && y > 241 && y < 281)
+            {
+                explod.Play();
+                declare4 = true;
+                lbl_crash.Location = new Point(x, y - 25);
+                lbl_fire.Location = new Point(lbl_hero.Location.X, lbl_hero.Location.Y);
+                timr_fire.Stop();
+                lbl_fire.Hide();
+                show = true;
+                points++;
+            }
+            if ((x + 40 > lbl_enemy6.Location.X) && y > 137 && y < 195)
+            {
+                explod.Play();
+                declare5 = true;
+                lbl_crash.Location = new Point(x, y - 25);
+                lbl_fire.Location = new Point(lbl_hero.Location.X, lbl_hero.Location.Y);
+                timr_fire.Stop();
+                lbl_fire.Hide();
+                show = true;
+                points++;
+            }
+
+
+
+            if (show == true)
+            {
+                lbl_crash.Show();
+                fire_show++;
+            }
+            if (fire_show > 50)
+            {
+                lbl_crash.Hide();
+                fire_show = 0;
+                show = false;
+                helicopter.PlayLooping();
+            }
+            lbl_points.Text = "" + points;
+
+
+
+
+
+
+
+
+        }
+
+        private void hero_collsion_Tick(object sender, EventArgs e)
+        {
+            int x = lbl_hero.Location.X;
+            int y = lbl_hero.Location.Y;
+
+            if  ((lbl_enemy1.Location.X < x+42 && y > 85 && y < 143) || (( y+40>lbl_enemy1.Location.Y && y< lbl_enemy1.Location.Y+10 )&& x > lbl_enemy1.Location.X && x<lbl_enemy1.Location.X+70 ) )
+            {
+                declare1 = true;
+                lbl_hero.Location = new Point(x, 42);
+                chances++;
+                lbl_crash.Location = new Point(x, y - 25);
+                show = true;
+                explod.Play();
+            }
+
+
+            if ((lbl_enemy2.Location.X < x + 42 && y > 175 && y < 235) || ((y + 40 > lbl_enemy2.Location.Y && y < lbl_enemy2.Location.Y + 10) && x > lbl_enemy2.Location.X && x < lbl_enemy2.Location.X + 70))
+            {
+                declare2 = true;
+                lbl_hero.Location = new Point(x, 42);
+                chances++;
+                lbl_crash.Location = new Point(x, y - 25);
+                show = true;
+                explod.Play();
+            }
+
+
+            if ((lbl_enemy3.Location.X < x + 42 && y > 275 && y < 335) || ((y + 40 > lbl_enemy3.Location.Y && y < lbl_enemy3.Location.Y + 10) && x > lbl_enemy3.Location.X && x < lbl_enemy3.Location.X + 70))
+            {
+                declare3 = true;
+                lbl_hero.Location = new Point(x, 42);
+                chances++;
+                lbl_crash.Location = new Point(x, y - 25);
+                show = true;
+                explod.Play();
+            }
+
+
+            if ((lbl_enemy4.Location.X < x + 42 && y > 225 && y < 288) || ((y + 40 > lbl_enemy4.Location.Y && y < lbl_enemy4.Location.Y + 10) && x > lbl_enemy4.Location.X && x < lbl_enemy4.Location.X + 70))
+            {
+                declare4 = true;
+                lbl_hero.Location = new Point(x, 42);
+                chances++;
+                lbl_crash.Location = new Point(x, y - 25);
+                show = true;
+                explod.Play();
+            }
+
+
+            if ((lbl_enemy6.Location.X < x + 42 && y > 125 && y < 180) || ((y + 40 > lbl_enemy6.Location.Y && y < lbl_enemy6.Location.Y + 10) && x > lbl_enemy6.Location.X && x < lbl_enemy6.Location.X + 70))
+            {
+                declare5 = true;
+                lbl_hero.Location = new Point(x, 42);
+                chances++;
+                lbl_crash.Location = new Point(x, y - 25);
+                show = true;
+                explod.Play();
+            }
+
+       
+
+            lbl_chances.Text = "" + chances;
+
+
+            if (chances == 5)
+            {
+                timr_bg.Stop();
+                timr_enemy.Stop();
+                timr_fire.Stop();
+                hero_collsion.Stop();
+                enemy1_collision.Stop();
+
+                MessageBox.Show("U Have MissUsed ur 5 Chances ... Game Over :( Play Again :D");
+                this.Close();
+            }
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
